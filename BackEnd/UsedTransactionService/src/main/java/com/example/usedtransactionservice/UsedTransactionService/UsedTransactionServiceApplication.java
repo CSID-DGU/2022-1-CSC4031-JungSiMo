389a05d31d6868f27973bf755853c7d49abb4ded
Sim_migyeong <80315847+Sim-mi-gyeong@users.noreply.github.com>
2022-05-09 11:22:37 +0900

    Modify Backend Project

---
+++ BackEnd/UsedTransactionService/src/main/java/com/example/usedtransactionservice/UsedTransactionService/UsedTransactionServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.usedtransactionservice.UsedTransactionService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsedTransactionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsedTransactionServiceApplication.class, args);
+	}
+
+}
---
+++ BackEnd/UsedTransactionService/src/test/java/com/example/usedtransactionservice/UsedTransactionService/UsedTransactionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.usedtransactionservice.UsedTransactionService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsedTransactionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
