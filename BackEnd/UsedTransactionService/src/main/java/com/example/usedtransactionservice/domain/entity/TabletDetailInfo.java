4b55f4f035a4e80824326dcc3ad7f3c392ad0cf8
Sim_migyeong <80315847+Sim-mi-gyeong@users.noreply.github.com>
2022-05-27 04:25:41 +0900

    이어폰/스마트워치/태블릿 상세정보 조회 API
    Method : POST
    
    Url : http://localhost:8080/api/v1/detail/info
    
    RequestBody
    
    {
        "itemId" : 6,
        "categoryId" : 4,
    
    }
    
    {
        "itemId": 6,
        "categoryId": 4,
        "itemImage": "https://shopping-phinf.pstatic.net/main_2941245/29412453621.20211028154519.jpg?type=f640",
        "bluetoothVersion": "블루투스5.2",
        "shape": "커널형",
        "channel": "스테레오",
        "purpose": "통화+음악",
        "soundQualityFirst": "액티브노이즈캔슬링",
        "soundQualitySecond": "공간음향",
        "operationFunc": "터치버튼",
        "waterproof": "생활방수(IPX4)",
        "microphoneYn": "마이크있음",
        "usageTime": "3.5시간",
        "terminal": "라이트닝"
    }

--- BackEnd/UsedTransactionService/src/main/java/com/example/usedtransactionservice/domain/entity/TabletDetailInfo.java
+++ BackEnd/UsedTransactionService/src/main/java/com/example/usedtransactionservice/domain/entity/TabletDetailInfo.java
@@ -2,9 +2,7 @@ package com.example.usedtransactionservice.domain.entity;
 
 import lombok.*;
 
-import javax.persistence.Entity;
-import javax.persistence.IdClass;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Builder
 @Getter @Setter
@@ -12,49 +10,72 @@ import javax.persistence.Table;
 @AllArgsConstructor
 @Table(name = "tablet_detail_info")
 @Entity
-//@IdClass(DetailInfoPK.class)
+@IdClass(DetailInfoPK.class)
 public class TabletDetailInfo {
 
+
+    @Id
+    @Column(name = "item_id")
     private Long itemId;
 
+    @Column(name = "category_id")
     private Long categoryId;
 
+    @Column(name = "item_image")
     private String itemImage;
 
+    @Column(name = "shape")
     private String shape;
 
+    @Column(name = "internet_connection")
     private String internetConnection;
 
+    @Column(name = "release_os")
     private String releaseOs;
 
+    @Column(name = "cpu_core")
     private String cpuCore;
 
+    @Column(name = "screen_size")
     private String screenSize;
 
+    @Column(name = "panel")
     private String panel;
 
+    @Column(name = "built_in_memory")
     private String builtInMemory;
 
+    @Column(name = "external_memory")
     private String externalMemory;
 
+    @Column(name = "audio")
     private String audio;
 
+    @Column(name = "front_camera_pixel")
     private String frontCameraPixel;
 
+    @Column(name = "rear_camera_pixel")
     private String rearCameraPixel;
 
-    private String blueTooth;
+    @Column(name = "bluetooth")
+    private String bluetooth;
 
+    @Column(name = "pen")
     private String pen;
 
+    @Column(name = "weight")
     private String weight;
 
+    @Column(name = "thickness")
     private String thickness;
 
+    @Column(name = "resolution")
     private String resolution;
 
+    @Column(name = "width")
     private String width;
 
+    @Column(name = "height")
     private String height;
 
 }
