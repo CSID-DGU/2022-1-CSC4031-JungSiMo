4acb5a3c5115b99183e0da84d77be3ed97beac23
Sim_migyeong <80315847+Sim-mi-gyeong@users.noreply.github.com>
2022-06-06 20:31:19 +0900

    노트북 상품 상세 정보 관련 추가

--- BackEnd/UsedTransactionService/src/main/java/com/example/usedtransactionservice/domain/entity/NotebookDetailInfo.java
+++ BackEnd/UsedTransactionService/src/main/java/com/example/usedtransactionservice/domain/entity/NotebookDetailInfo.java
@@ -14,7 +14,6 @@ import javax.persistence.Table;
 @AllArgsConstructor
 @Table(name = "notebook_detail_info")
 @Entity
-//@IdClass(DetailInfoPK.class)
 public class NotebookDetailInfo {
 
     @Id
@@ -26,4 +25,41 @@ public class NotebookDetailInfo {
 
     @Column(name = "item_image")
     private String itemImage;
+
+    @Column(name = "screen_size")
+    private String screenSize;
+
+    @Column(name = "weight")
+    private String weight;
+
+    @Column(name = "width")
+    private String width;
+
+    @Column(name = "height")
+    private String height;
+
+    @Column(name = "os")
+    private String os;
+
+    @Column(name = "core")
+    private String core;
+
+    @Column(name = "ram")
+    private String ram;
+
+    @Column(name = "resolution")
+    private String resolution;
+
+    @Column(name = "panel")
+    private String panel;
+
+    @Column(name = "cable")
+    private String cable;
+
+    @Column(name = "bluetooth")
+    private String bluetooth;
+
+    @Column(name = "built_in_memory")
+    private String builtInMemory;
+
 }
